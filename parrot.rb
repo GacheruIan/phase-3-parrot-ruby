a186cf014cd17589980ec75557251afebcaf85f4
Gacheruian <ian.gacheru@student.moringaschool.com>
2022-10-20 19:55:41 +0300

    Done assignment

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,9 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+
+def parrot (my_string = "Squawk!")
+    puts "#{my_string}"
+    "#{my_string}"
+    
+end
+parrot(my_string = "Squawk!")
